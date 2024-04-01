15f63c2cc6dc3380a5c94ffee9512ad5759556fe
kimzeze <96248861+kimzeze@users.noreply.github.com>
2024-04-01 16:07:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,18 +1,14 @@
-def collatz(num, ans):
-    # 입력 숫자가 1이면 ans를 반환하고 종료
-    if num == 1:
-        return ans
-    # 만약 500번 이상될 때 까지 반복한다면 -1을 반환하고 종료
+def solution(num):
+    ans = 0
+    # 조건 500번, 1이될떄까지
+    while num != 1 and ans < 500:
+        # num이 짝수인 경우
+        if num % 2 == 0:
+            num //= 2
+        # num이 홀수인 경우
+        else:
+            num = num * 3 + 1
+        ans += 1
     if ans == 500:
         return -1
-    
-    # 짝수일 경우
-    if num % 2 == 0:
-        return collatz(num // 2, ans + 1)
-    # 홀수일 경우
-    elif num % 2 == 1:
-        return collatz(num * 3 + 1, ans + 1)
-
-def solution(num):
-    return collatz(num, 0)
-
+    return ans
