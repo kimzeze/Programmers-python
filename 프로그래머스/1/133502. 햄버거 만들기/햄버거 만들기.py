ec202b551842761413492af5d94fb2c7659e156d
kimzeze <96248861+kimzeze@users.noreply.github.com>
2024-05-03 16:00:27 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,14 +1,3 @@
 def solution(ingredient):
-    stack = []
-    burger_count = 0
-    
-    for i in ingredient:
-        stack.append(i)
-        
-        if len(stack) >= 4:
-            if stack[-4:] == [1, 2, 3, 1]:
-                for _ in range(4):
-                    stack.pop()
-                burger_count += 1
-    
-    return burger_count
+    answer = 0
+    return answer
