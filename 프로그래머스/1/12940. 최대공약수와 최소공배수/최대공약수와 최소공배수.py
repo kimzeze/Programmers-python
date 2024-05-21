54ec5f27c1df7fd462cb3baf528650f34161f2b3
kimzeze <96248861+kimzeze@users.noreply.github.com>
2024-05-21 16:26:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -3,12 +3,14 @@ def solution(n, m):
     
     min_num = min(n,m)
     
+    # n, m 을 나누어서 나머지가 0이 되는 가장 큰 값 => 최소공배수
     for i in range(min_num, 0, -1):
         if n % i == 0 and m % i == 0:
             answer.append(i)
             break
 
-            
+    
+    # n * m 을 최대공약수로 나눈 값 => 최대공약수
     answer.append((n * m) // answer[0])
     
     return answer
