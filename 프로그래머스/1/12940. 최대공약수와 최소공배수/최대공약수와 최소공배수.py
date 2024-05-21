0bdf2aa942690d43f718af5b33faa24004353657
kimzeze <96248861+kimzeze@users.noreply.github.com>
2024-05-21 16:31:10 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -3,7 +3,8 @@ def solution(n, m):
     
     min_num = min(n,m)
     
-    # n, m 을 나누어서 나머지가 0이 되는 가장 큰 값 => 최소공배수
+    # n, m 을 나누어서 둘다 나머지가 0이 되는 가장 큰 값 => 최소공배수
+    # (n, m)의 최솟값부터 시작해서 0까지 하나씩 나눠봄
     for i in range(min_num, 0, -1):
         if n % i == 0 and m % i == 0:
             answer.append(i)
